70ea5ae3fd3f238af6ea86679468f37b61667a83
Stéphane Busso <stephane.busso@gmail.com>
2016-06-19 17:03:31 +0700

    first running version

--- main.go
+++ main.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"math/rand"
 	"net/http"
-	"os"
+	"runtime"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/engine/fasthttp"
@@ -18,9 +17,15 @@ type Banner struct {
 	Url   string
 }
 
+type Site struct {
+	settings map[string]string
+	Zones    map[string]string
+}
+
 type Config struct {
-	Zones   map[string][]int
-	Banners map[int]Banner
+	Defaults map[string]string
+	Sites    map[string]Site
+	Banners  map[string][]Banner
 }
 
 func (c *Config) parse(data []byte) error {
@@ -30,7 +35,7 @@ func (c *Config) parse(data []byte) error {
 // TODO: Pre generate Banners
 // TODO: Use goroutine
 
-func sample(a []int) int {
+func sample(a []Banner) Banner {
 	r := rand.Intn(len(a))
 	return a[r]
 }
@@ -39,34 +44,67 @@ func generateBanner(image string, url string) string {
 	return "<a href='" + url + "'><img src='" + image + "'/></a>"
 }
 
-func getBanner(zoneID string, c *Config) string {
-	b := c.Banners[sample(c.Zones[zoneID])]
-	return generateBanner(b.Image, b.Url)
+func getBanner(site string, zoneID string, c *Config) string {
+	b := sample(c.Banners[c.Sites[site].Zones[zoneID]])
+	return generateBanner(c.Defaults["base_uri"]+b.Image, b.Url)
 }
 
 func main() {
-	data, err := ioutil.ReadFile("config.yml")
-	if err != nil {
-		println("File does not exist:", err.Error())
-		os.Exit(1)
-	}
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	data := `
+defaults:
+  base_uri: http://static.turfmedia.com/
+sites:
+  ph:
+    settings:
+      affiliate-id: 2a848818
+
+    zones:
+      top: banner728x90
+      middle: banner728x90
+banners:
+  banner728x90: #skyscraper
+    -
+      image: turfistar/3481d796.png
+      url: "http://turfistar.com/#a_aid=2a848818&a_bid=3481d796"
+    -
+      image: pronostic-facile/0eca889d.png
+      url: "http://www.pronostic-facile.fr?a_aid=2a848818&a_bid=0eca889d"
+    -
+      image: gazette-turf/8d3376b4.png
+      url: "http://gazette-turf.fr/#a_aid=2a848818&a_bid=8d3376b4"
+`
+
+	// data, err := ioutil.ReadFile("config.yml")
+	// if err != nil {
+	// 	println("File does not exist:", err.Error())
+	// 	os.Exit(1)
+	// }
+
+	// data, err := Asset("config.yml")
+	// if err != nil {
+	// 	println("config not found:", err.Error())
+	// 	os.Exit(1)
+	// }
 
 	var config Config
-	if err := config.parse(data); err != nil {
+	if err := config.parse([]byte(data)); err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Printf("%#v\n", config)
 
 	e := echo.New()
-	e.GET("/zones/:id", func(c echo.Context) error {
+	e.GET("/zones/:site/:id", func(c echo.Context) error {
 		zone := c.Param("id")
-		if config.Zones[zone] != nil {
-			return c.String(http.StatusOK, getBanner(zone, &config))
+		site := c.Param("site")
+		if config.Sites[site].Zones[zone] != "" {
+			return c.String(http.StatusOK, getBanner(site, zone, &config))
 		}
 		return c.String(http.StatusNotFound, "Not Found")
 	})
-	e.Static("/", "public/index.html")
+	e.Static("/", "ads/index.html")
 	e.Run(fasthttp.New(":1323"))
 
 }
